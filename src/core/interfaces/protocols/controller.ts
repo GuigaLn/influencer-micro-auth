07281d3b4b9eef1c3ea42d64a09e4a077a5a7e57
GuilhermeNallon <116029163+GuilhermeNallon@users.noreply.github.com>
2024-08-28 19:18:35 -0300

    refactor: Controller

--- src/core/interfaces/protocols/controller.ts
+++ src/core/interfaces/protocols/controller.ts
@@ -1,5 +1,16 @@
 import { Response } from "./http";
+import { makeValidationComposite } from "@/core/validators/factories/make-validation-composite";
+import { ValidationField } from "@/core/validators/protocols/validation-field";
 
 export abstract class Controller<T> {
+  constructor(private readonly validationField: ValidationField[]) {}
+
   abstract perform(input: T): Promise<Response>;
+
+  validate(input: T) {
+    return makeValidationComposite.validade(
+      input as Record<string, any>,
+      this.validationField,
+    );
+  }
 }
