89aefac8c2d02b87e093e3f2d2d25bbeb098b4cf
GuilhermeNallon <116029163+GuilhermeNallon@users.noreply.github.com>
2024-08-28 17:33:54 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+const config = {
+  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
+  coverageDirectory: "coverage",
+  coverageProvider: "babel",
+  moduleNameMapper: {
+    "@/tests/(.+)": "<rootDir>/tests/$1",
+    "@/(.+)": "<rootDir>/src/$1",
+  },
+  testMatch: ["<rootDir>/**/*.spec.ts"],
+  roots: ["<rootDir>/src", "<rootDir>/tests"],
+  transform: {
+    "\\.ts$": "ts-jest",
+  },
+  clearMocks: true,
+};
+
+module.exports = config;
