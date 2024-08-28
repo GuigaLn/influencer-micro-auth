250fc796d3b3d5930d46e29e764ce022fbc51092
GuilhermeNallon <116029163+GuilhermeNallon@users.noreply.github.com>
2024-08-28 17:21:50 -0300

    chore: add eslint and prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import jest from "eslint-plugin-jest";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default [
+  {
+    ignores: ["dist/"],
+  },
+  { files: ["/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["test//*.{js,mjs,cjs,ts}"],
+    ...jest.configs["flat/recommended"],
+    rules: {
+      ...jest.configs["flat/recommended"].rules,
+      "jest/prefer-expect-assertions": "off",
+    },
+  },
+  // you can also configure jest rules in other objects, so long as some of the files match
+  {
+    files: ["test/"],
+    rules: { "jest/prefer-expect-assertions": "off" },
+  },
+  eslintPluginPrettierRecommended,
+];
