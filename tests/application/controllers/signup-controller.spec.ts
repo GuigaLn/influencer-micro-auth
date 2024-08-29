503b3f8c96d501867de8fe9ef6b9e862bb3cd801
GuilhermeNallon <116029163+GuilhermeNallon@users.noreply.github.com>
2024-08-29 19:13:15 -0300

    test: simple test SignUpController

---
+++ tests/application/controllers/signup-controller.spec.ts
@@ -0,0 +1,75 @@
+import { SignupController } from "@/core/interfaces/controllers/signup-controller";
+
+interface SutTypes {
+  sut: SignupController;
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new SignupController();
+
+  return {
+    sut,
+  };
+};
+
+describe("SignUpController", () => {
+  it("Should return 400 with no name is provided", async () => {
+    const { sut } = makeSut();
+    const httpRequest: any = {
+      email: "any_email@email.com",
+      password: "any_password",
+    };
+
+    const httpResponse = await sut.perform(httpRequest);
+
+    const expected = {
+      statusCode: 400,
+      body: {
+        errors: ["name is not a valid string"],
+      },
+    };
+
+    expect(httpResponse.statusCode).toBe(expected.statusCode);
+    expect(httpResponse.body).toEqual(expected.body);
+  });
+
+  it("Should return 400 with no email is provided", async () => {
+    const { sut } = makeSut();
+    const httpRequest: any = {
+      name: "any_name",
+      password: "any_password",
+    };
+
+    const httpResponse = await sut.perform(httpRequest);
+
+    const expected = {
+      statusCode: 400,
+      body: {
+        errors: ["email is not a valid email"],
+      },
+    };
+
+    expect(httpResponse.statusCode).toBe(expected.statusCode);
+    expect(httpResponse.body).toEqual(expected.body);
+  });
+
+  it("Should return 400 with no passowrd is provided", async () => {
+    const { sut } = makeSut();
+    const httpRequest: any = {
+      email: "any_email@email.com",
+      name: "any_name",
+    };
+
+    const httpResponse = await sut.perform(httpRequest);
+
+    const expected = {
+      statusCode: 400,
+      body: {
+        errors: ["password is not a valid string"],
+      },
+    };
+
+    expect(httpResponse.statusCode).toBe(expected.statusCode);
+    expect(httpResponse.body).toEqual(expected.body);
+  });
+});
