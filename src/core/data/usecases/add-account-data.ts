262c79210f76a7574798fcba27f2b60d94a06521
GuilhermeNallon <116029163+GuilhermeNallon@users.noreply.github.com>
2024-08-30 13:09:11 -0300

    refactor: AddAccountData add Encrypter and UUIDGenerator

--- src/core/data/usecases/add-account-data.ts
+++ src/core/data/usecases/add-account-data.ts
@@ -1,17 +1,36 @@
 import { AddAccount } from "@/core/domain/usecases/add-account";
 import { AddAccountDto } from "@/core/domain/models/add-account.dto";
 import { AccountRepository } from "@/core/domain/repositories/account-repository";
+import { Encrypter } from "@/core/domain/cryptography/encrypter";
+import { UUIDGenerator } from "@/core/domain/services/uuid-generator";
 
 export class AddAccountData implements AddAccount {
-  constructor(private readonly accountRepository: AccountRepository) {}
+  constructor(
+    private readonly accountRepository: AccountRepository,
+    private readonly encrypter: Encrypter,
+    private readonly uuidGenerator: UUIDGenerator,
+  ) {}
 
-  async add(account: Omit<AddAccountDto, "id">): Promise<boolean> {
-    const checkByEmail = await this.accountRepository.checkByEmail(
-      account.email,
-    );
+  async add({
+    name,
+    email,
+    password,
+  }: Omit<AddAccountDto, "id">): Promise<boolean> {
+    const checkByEmail = await this.accountRepository.checkByEmail(email);
     if (checkByEmail) {
       return false;
     }
-    return true;
+
+    const uuid = this.uuidGenerator.generate();
+    const hashedPassword = await this.encrypter.encrypt(password);
+    const account: AddAccountDto = {
+      id: uuid,
+      name,
+      email,
+      password: hashedPassword,
+    };
+
+    const created = await this.accountRepository.add(account);
+    return created;
   }
 }
